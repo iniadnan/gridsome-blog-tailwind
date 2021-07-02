31215cefa3c0585b6a8dd8357521415d9fb5ed6c
iniadnan <muh.adnan.muttaqin@gmail.com>
2021-07-02 16:30:49 +0700

    sulit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
-import './assets/css/dist/tailwind.css'
+import './assets/css/compile/tailwind.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
